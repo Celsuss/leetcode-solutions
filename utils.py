804d8336b11e4a98dd3c58f2277cdf7ee09b7938
celsuss <jenslorden@gmail.com>
2022-10-12 17:12:22 +0200

    Updates to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
 
+##################
+# Util Functions #
+##################
+
+def removeTrailingNonesFromArray(array):
+    for i in range(len(array), 0, -1):
+        if array[i-1] is None:
+            array.pop(i-1)
+        else:
+            return array
+
+    return array
 
 #############
 # Tree Node #
@@ -12,19 +24,24 @@ class TreeNode:
 def createTreeFromArray(array):
     if array is None or len(array) == 0:
         return None
+
+
+    def createNodeBreathFirst(i):
+        if i >= len(array):
+            return
+        return
         
-    def createNode(i, offset):
+    def createNodeDepthFirst(i, offset):
         if i >= len(array) or array[i] == None:
             return None
 
         node = TreeNode(array[i])
         i-= offset
-        
-        node.left = createNode(i * 2 + 1, 0)
-        node.right = createNode(i * 2 + 2, 0 if node.left is not None else offset + 1)
-
+        node.left = createNodeDepthFirst(i * 2 + 1, 0)
+        node.right = createNodeDepthFirst(i * 2 + 2, 0 if node.left is not None else offset + 1)
         return node
-    return createNode(0, 0)
+
+    return createNodeDepthFirst(0, 0)
 
 def createArrayFromTree(root: TreeNode):
     if root is None or root.val is None:
@@ -34,13 +51,18 @@ def createArrayFromTree(root: TreeNode):
     queue = [root]
     def breadthFirst(node: TreeNode):
         if node is None:
-            return
-        res.append(node.val)
-        queue.append(node.left)
-        queue.append(node.right)
+            res.append(None)
+        else:
+            res.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
         queue.pop(0)
-        breadthFirst(queue[0])
+
+        if len(queue) > 0:
+            breadthFirst(queue[0])
+
     breadthFirst(root)
+    removeTrailingNonesFromArray(res)
     return res
 
 #############
