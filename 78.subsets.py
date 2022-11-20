e240d2199d8dc6d1fb9d96250d24feb60c2aa8a5
celsuss <jenslorden@gmail.com>
2022-11-20 11:20:29 +0100

    Small improbments to 78

--- 78.subsets.py
+++ 78.subsets.py
@@ -7,44 +7,21 @@
 # @lc code=start
 class Solution:
     def subsets(self, nums: list[int]) -> list[list[int]]:
-        if len(nums) == 0:
-            return None
+        if nums is None or len(nums) == 0:
+            return []
 
         res = []
-        self.getSubsets(nums, 0, [], res)
-        # subsets = self.backPropegate(nums)
+        self.getSubsets(nums, [], res)
 
         return res
 
-    def getSubsets(self, nums: list[int], start: int, current: list[int], subsets: list[list[int]]):
+    def getSubsets(self, nums: list[int], current: list[int], subsets: list[list[int]]):
         subsets.append(current[:])
-        for i in range(start, len(nums)):
-            current.append(nums[i])
-            self.getSubsets(nums, i+1, current, subsets)
-            current.pop()
 
-            continue
-
-        return subsets
-
-    def backPropegate(self, nums: list[int]) -> list[list[int]]:
-        if len(nums) <= 1:
-            return None
-
-        subsets = []
-        # Backpropegate by removing one element at a time
         for i in range(len(nums)):
-            new_set = [nums[j] for j in range(len(nums)) if j != i]
-            subsets.append(new_set)
-
-            new_sets = self.backPropegate(new_set)
-            if new_sets is None:
-                continue
-
-            for i in range(len(new_sets)):
-                subsets.append(new_sets[i])
-
-            continue
+            current.append(nums[i])
+            self.getSubsets(nums[i+1:], current, subsets)
+            current.pop()
 
         return subsets
         
